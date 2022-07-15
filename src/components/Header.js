481b46d72f3be61d4d91f24d2b9225cfe97977a6
elcharitas <jonathanirhodia@gmail.com>
2022-07-15 14:50:54 +0100

    feat: add icons to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,13 +10,16 @@ export const Header = ({ pageTitle }) => {
       <Head>
         <title>{pageTitle ? `${pageTitle} - ${title}` : title}</title>
       </Head>
-      <div className="fixed z-10 py-5 inset-x-0 top-0 z-20 transition-all">
+      <div
+        className="fixed bg-white z-10 py-5 inset-x-0 top-0 z-20 transition-all"
+        style={{ backdropFilter: "blur(5px)" }}
+      >
         <div
           className="relative max-w-[1240px] 2xl:max-w-[1472px] mx-auto px-4 md:px-6 lg:px-10 xl:px-0"
           aria-label="Global"
         >
           <nav
-            className="relative flex items-center justify-between rounded-full text-white px-8 py-3 bg-secondary-1"
+            className="relative flex items-center justify-between rounded-full text-white px-4 py-3 bg-secondary-1"
             aria-label="Global"
           >
             <Link href="/" shallow className="flex items-center" role="button">
@@ -26,7 +29,6 @@ export const Header = ({ pageTitle }) => {
             <div className="flex items-center -mr-1 hidden">
               <button
                 className="inline-flex items-center justify-center p-2 text-white rounded-full bg-black focus:outline-none"
-                id="headlessui-popover-button-undefined"
                 type="button"
                 aria-expanded="false"
               >
@@ -56,8 +58,20 @@ export const Header = ({ pageTitle }) => {
                 {accounts.length > 0 ? (
                   accounts[0].address
                 ) : (
-                  <span>
-                    <span className="xs:hidden">Connect Wallet</span>
+                  <span className="flex items-center">
+                    <span className="xs:hidden">Connect Wallet &nbsp;</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      width="24"
+                      height="24"
+                    >
+                      <g xmlns="http://www.w3.org/2000/svg">
+                        <path d="M27,10H6c-1.1,0-2-0.9-2-2s0.9-2,2-2h4c0.6,0,1-0.4,1-1s-0.4-1-1-1H6C3.8,4,2,5.8,2,8v16c0,2.2,1.8,4,4,4h21   c1.7,0,3-1.3,3-3V13C30,11.3,28.7,10,27,10z M28,15.8v6.4c-0.3-0.1-0.6-0.2-1-0.2h-4c-1.7,0-3-1.3-3-3s1.3-3,3-3h4   C27.4,16,27.7,15.9,28,15.8z" />
+                        <path d="M7,7C6.4,7,6,7.4,6,8s0.4,1,1,1h4c0,0,0,0,0,0s0,0,0,0h15c0.3,0,0.6-0.2,0.8-0.4c0.2-0.3,0.2-0.6,0.1-0.9l-2-5   c-0.2-0.5-0.8-0.8-1.3-0.6L10.8,7H7z" />
+                        <path d="M25,18h-2c-0.6,0-1,0.4-1,1s0.4,1,1,1h2c0.6,0,1-0.4,1-1S25.6,18,25,18z" />
+                      </g>
+                    </svg>
                   </span>
                 )}
               </a>
