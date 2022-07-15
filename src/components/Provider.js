6c37f8485dbf300bc42f014935bddf2c2868631a
elcharitas <jonathanirhodia@gmail.com>
2022-07-15 09:55:56 +0100

    feat: persist state to localstorage

--- src/components/Provider.js
+++ src/components/Provider.js
@@ -1,10 +1,18 @@
 import { useImmer } from "use-immer";
 import { defaults, AppContext } from "../hooks";
+import { store } from "../utils";
 
 export const AppProvider = ({ children }) => {
-  const [state, update] = useImmer({
-    accounts: [],
-  });
+  const [state, setState] = useImmer(
+    store("app") || {
+      accounts: [],
+    }
+  );
+
+  const update = (fn) => {
+    setState(fn);
+    store("app", state);
+  };
 
   const reset = (key) => {
     update((draft) => {
