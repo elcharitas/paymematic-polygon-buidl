8d33b09eded4003d7beea6ad98f6c94cb7aac14d
elcharitas <jonathanirhodia@gmail.com>
2022-07-15 08:07:05 +0100

    feat: use Open Sans font

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,11 +6,11 @@ export default function PMMDocument() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link rel="icon" type="image/png" href="/static/images/logo.png" />
         <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,600;1,800&display=swap"
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;700;900&display=swap"
         />
+        <link rel="icon" type="image/png" href="/static/images/logo.png" />
       </Head>
       <body className="paymematic">
         <Main />
