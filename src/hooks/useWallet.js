b41eb7a76b90cb6dbd34b6648f29eadef0b33a0a
elcharitas <jonathanirhodia@gmail.com>
2022-07-15 10:50:09 +0100

    fix: refactor wallet auth hook

--- src/hooks/useWallet.js
+++ src/hooks/useWallet.js
@@ -1,15 +1,34 @@
-import { utils } from "ethers";
 import { useSnackbar } from "react-simple-snackbar";
+import { provider, handleLogs, formatBigNumber, formatAddress } from "../utils";
 
 import { useApp } from "./useApp";
 
 export function useWallet() {
-  const { accounts, reset } = useApp();
+  const { chainId, setAccounts } = useApp();
   const [snackbar] = useSnackbar();
 
-  const connectWallet = async () => {};
+  const connectWallet = async () => {
+    provider
+      .ethers(chainId, handleLogs.bind({}, snackbar))
+      .then(async (eth3) => {
+        if (!eth3) return;
+        const accountList = [];
+        for await (const address of await eth3.listAccounts()) {
+          accountList.push({
+            fullAddress: address,
+            address: formatAddress(address),
+            balance: formatBigNumber(await eth3.getBalance(address)),
+          });
+        }
+        setAccounts(accountList);
+      });
+  };
 
-  const disconnectWallet = async () => {};
+  const disconnectWallet = async () => {
+    await provider.disconnect().then(() => {
+      snackbar("Wallet has been disconnected");
+    });
+  };
 
   return [connectWallet, disconnectWallet];
 }
