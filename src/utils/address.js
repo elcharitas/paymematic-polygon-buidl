d8a1439f29b0dd2d9594258ff2ec7730404e8a53
elcharitas <jonathanirhodia@gmail.com>
2022-07-16 08:08:10 +0100

    feat: add util to check null addresses

--- src/utils/address.js
+++ src/utils/address.js
@@ -3,3 +3,6 @@ export const etherScanUrl = (address) =>
 
 export const formatAddress = (address) =>
   address.substring(0, 6) + "..." + address.substring(address.length - 4);
+
+export const isNullAddress = (address) =>
+  address && address.indexOf("0x00") === 0;
