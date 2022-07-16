58f5dccb064ea11c509edf1076cf355baac58f98
elcharitas <jonathanirhodia@gmail.com>
2022-07-16 14:41:46 +0100

    feat: add util to get gravatar url from addresses

--- src/utils/address.js
+++ src/utils/address.js
@@ -6,3 +6,6 @@ export const formatAddress = (address) =>
 
 export const isNullAddress = (address) =>
   address && address.indexOf("0x00") === 0;
+
+export const getGravatar = (address) =>
+  `https://www.gravatar.com/avatar/${address}?r=g&d=robohash&s=300`;
